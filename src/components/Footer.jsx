a7aa0cc5a930126adaaf6eaaba06a0f23e7eb02a
AlexSkilled <olegthander@yandex.ru>
2023-10-10 17:55:37 +0300

    changed name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,8 @@
 const Footer = () => {
   return (
     <div className='container-max text-center py-4 border-t'>
-      Foody, made by{' '}
-      <a
-        href='https://dineshrout.netlify.app'
-        target={'_blank'}
-        className='text-orange-500'
-      >
-        Dinesh Rout
-      </a>
+     itmo yandex metrics project
+
     </div>
   );
 };
