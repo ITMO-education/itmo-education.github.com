0e36b82e2942e65f552df21934364f1af1c7957c
AlexSkilled <olegthander@yandex.ru>
2023-10-19 15:23:04 +0300

    ab expirements random

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,8 @@ import { useState } from "react";
 
 const Home = () => {
   // Эксперимент 1 - показываем лучшие предложения в начале
-  // const [getExperiment, setExperiment] = useState(Math.floor(Math.random()*2))
-  const [getExperiment, setExperiment] = useState(1)
+  const [getExperiment, setExperiment] = useState(Math.floor(Math.random()*2))
+  // const [getExperiment, setExperiment] = useState(1)
 
   return <Body expirement={getExperiment}/>;
 };
